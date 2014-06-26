c798d2d60e84d9271031c2252da25924dbb9905f
Nicolas Trangez <ikke@nicolast.be>
2014-06-26 20:59:53 +0200

    Add `tcpProxy` `Application`

--- Demo.hs
+++ Demo.hs
@@ -9,7 +9,7 @@ import Control.Lens (view)
 import Pipes
 
 import Network.SockMock (Application, remoteAddress, run, tcpServer)
-import Network.SockMock.Policies (blackhole, disconnect, disconnectLater)
+import Network.SockMock.Policies (blackhole, disconnect, disconnectLater, tcpProxy)
 
 sayHello :: Application
 sayHello _ cons = do
@@ -28,4 +28,5 @@ main = run servers
               , tcpServer "8081" blackhole
               , tcpServer "8082" sayHello
               , tcpServer "8083" (disconnectLater 1000000)
+              , tcpServer "8084" (tcpProxy "127.0.0.1" "8082")
               ]
--- src/Network/SockMock/Policies.hs
+++ src/Network/SockMock/Policies.hs
@@ -4,14 +4,17 @@ module Network.SockMock.Policies (
       blackhole
     , disconnect
     , disconnectLater
+    , tcpProxy
     ) where
 
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (forkIO, threadDelay)
 
 import qualified Data.Text as T
 
 import Pipes
 import qualified Pipes.Prelude as PP
+
+import Pipes.Network.TCP.Safe
 
 import Network.SockMock (Application, logMessage, pipeHandler)
 
@@ -32,4 +35,19 @@ disconnectLater d _ _ = do
     msg = T.concat [ "Disconnecting after "
                    , T.pack $ show d
                    , "us"
+                   ]
+
+tcpProxy :: HostName -> ServiceName -> Application
+tcpProxy host service prod cons = do
+    logMessage msg
+    liftIO $ runSafeT $
+        connect host service (\(sock, _) -> liftIO $ do
+            _ <- forkIO $ runEffect $ fromSocket sock bufferSize >-> cons
+            runEffect $ prod >-> toSocket sock)
+  where
+    bufferSize = 4096
+    msg = T.concat [ "Proxying to "
+                   , T.pack host
+                   , ":"
+                   , T.pack service
                    ]
